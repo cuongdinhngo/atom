57267fd9d26e58c18281339c8311c07674b8613e
AT-Cuong.Ngo <cuong.ngo@asiantech.vn>
2020-03-17 22:05:33 +0700

    set seperately system path configuration

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,17 +1,5 @@
 <?php
 
-error_reporting(E_ALL & ~E_NOTICE);
-
-define('DOC_ROOT', $_SERVER["DOCUMENT_ROOT"]);
-define('CONFIG_PATH', DOC_ROOT.'/../config/');
-define('ROUTE_PATH', DOC_ROOT.'/../app/Routes/');
-define('CONTROLLER_PATH', DOC_ROOT.'/../app/Controllers/');
-define('VIEW_PATH', DOC_ROOT.'/../resources/views/');
-define('STORAGE_PATH', DOC_ROOT.'/../storage/');
-define('LOG_PATH', DOC_ROOT.'/../storage/logs/');
-define('RESOURCES_PATH', DOC_ROOT.'/../resources/');
-define('ASSETS_PATH', DOC_ROOT.'/assets/');
-
 if (!function_exists('config')) {
     /**
      * Load configured files
