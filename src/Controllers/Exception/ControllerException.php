487f423e946d796d7ff7fd340fd9111e748ce5c2
Ngo Dinh Cuong <cuongnd@nitrotech.asia>
2020-08-03 16:39:53 +0700

    apply Doctrine

--- src/Controllers/Exception/ControllerException.php
+++ src/Controllers/Exception/ControllerException.php
@@ -6,4 +6,5 @@ class ControllerException extends \Exception
 {
     const ERR_MSG_INVALID_CONTROLLER = "Controller Not Found";
     const ERR_MSG_ACTION_FAIL = "Action Failed";
+    const ERR_MSG_DOCTRINE_NOT_USE = "Doctrine Not Used";
 }
